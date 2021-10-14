1bb5094639f5abef133ef4956e7e74d3e8389dc1
YousofAboElkhair <88256260+YousofAboElkhair@users.noreply.github.com>
2021-10-14 14:55:52 +0200

    Update and rename preprocess.py to reading.py

---
+++ reading.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+concrete = pd.read_csv('../input/dl-course-data/concrete.csv')
+df = concrete.copy()
+
+df_train = df.sample(frac=0.7, random_state=0)
+df_valid = df.drop(df_train.index)
+
+X_train = df_train.drop('CompressiveStrength', axis=1)
+X_valid = df_valid.drop('CompressiveStrength', axis=1)
+y_train = df_train['CompressiveStrength']
+y_valid = df_valid['CompressiveStrength']
+
+input_shape = [X_train.shape[1]]
